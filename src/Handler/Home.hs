5382e33b4e430c141e61c6603e03e3b254311fc5
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-10-31 23:50:36 +0000

    page 2 e 3

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -8,8 +8,22 @@ module Handler.Home where
 
 import Import
 import Data.FileEmbed (embedFile)
+import Text.Lucius
+import Text.Julius
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+
+getPage3R :: Handler Html
+getPage3R = do 
+    defaultLayout $ do 
+        $(whamletFile "templates/page3.hamlet")
+
+getPage2R :: Handler Html
+getPage2R = do 
+    defaultLayout $ do 
+        $(whamletFile "templates/page2.hamlet")
+        $(luciusFile "templates/page2.lucius")
+        $(juliusFile "templates/page2.julius")
 
 getPage1R :: Handler Html
 getPage1R = do 
