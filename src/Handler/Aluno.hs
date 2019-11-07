5a75ef6db4f8691a88fb42ec4db289e83ce65637
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-11-07 23:57:34 +0000

    TABELA ALUNO

---
+++ src/Handler/Aluno.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+import Text.Lucius
+import Text.Julius
+import Database.Persist.Postgresql
+
+formAluno :: Form Aluno 
+formAluno = renderBootstrap $ Aluno 
+    <$> areq textField "Nome: " Nothing 
+    <*> areq textField "RA: "   Nothing 
+    <*> areq dayField  "Data: " Nothing 
+
+getAlunoR :: Handler Html 
+getAlunoR = do 
+    (widget, enctype) <- generateFormPost formAluno 
+    msg <- getMessage
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
+        [whamlet|
+            $maybe mensa <- msg
+                <div>
+                    ^{mensa}
+            
+            <h1>
+                CADASTRO DE ALUNO
+            
+            <form method=post action=@{AlunoR}>
+                ^{widget}
+                <button>
+                    Cadastrar
+        |]
+
+postAlunoR :: Handler Html 
+postAlunoR = do 
+    ((result,_),_) <- runFormPost formAluno 
+    case result of 
+        FormSuccess aluno -> do 
+            runDB $ insert aluno 
+            setMessage [shamlet|
+                <h2>
+                    REGISTRO INCLUIDO
+            |]
+            redirect AlunoR
+        _ -> redirect HomeR
+
+
